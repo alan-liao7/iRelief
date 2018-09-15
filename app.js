ae1f5f6610d9450c6c873f7e9674c7a63dbaa32e
Alan Liao <alan.liao@utdallas.edu>
2018-09-15 03:19:45 -0400

    Added pages

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var createError = require('http-errors');
-//var admin = require('firebase-admin');
 var express = require('express');
 var path = require('path');
+//var favicon = require('serve-favicon');
+//var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var bodyParser = require('body-parser');
+//var hbs = require('express-handlebars');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes = require('./routes/index');
 
 var app = express();
 
@@ -14,36 +14,46 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handler
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
-// Listen for search requests
-app.post('/search', function(req, res){
-    console.log('POST /search');
-	res.set('Content-Type', 'text/plain');
-	res.send(`You sent: ${req.body} to Express`);
-});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/',function(req, res, next){
+	res.render('index');
+});
+  
+router.get('/search',function(req, res, next){
+	res.render('search');
+});
+  
+router.get('/sos',function(req, res, next){
+	res.render('sos');
 });
 
 module.exports = router;
